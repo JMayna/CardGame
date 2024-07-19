90af5f7b54b7f8f7aa092aeb12e7ae3a0d76e1c5
Jordan Maynard <j.maynard.20122@gmail.com>
2024-07-18 22:44:15 -0400

    0.1 Black and White Deck

---
+++ Cards/White Cards/WhiteCard.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+var attack = 0
+var defense = 0
+var cardname = "name"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var attack_label = get_node("Sprite2D/attackLabel")
+	var defense_label = get_node("Sprite2D/defenseLabel")
+	var name_label = get_node("Sprite2D/nameLabel")
+	
+	attack = 6
+	defense = 4
+	cardname = "Centuar"
+	
+	if attack_label:
+		attack_label.text = "Attack: " + str(attack) 
+	if defense_label:
+		defense_label.text = "Defense: " + str(defense)
+	if name_label:
+		name_label.text = cardname
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
